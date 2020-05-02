4b98cc395b076bc1d388e742f4f36d76abb1982e
dimolto <oshiroyasu52@gmail.com>
2020-05-02 14:27:21 +0900

    Add ResetController

---
+++ oneDayGameClient/Assets/oneDayGame/Scripts/ResetController.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+
+public class ResetController : MonoBehaviour
+{
+    class StartInfo
+    {
+        private Rigidbody rigidbody;
+
+        private Vector3 startPosition;
+
+        public StartInfo(Rigidbody rigidbody)
+        {
+            this.rigidbody = rigidbody;
+            startPosition = rigidbody.transform.position;
+        }
+
+        public void Reset()
+        {
+            rigidbody.velocity = Vector3.zero;
+            rigidbody.angularVelocity = Vector3.zero;
+            rigidbody.transform.position = startPosition;
+            rigidbody.gameObject.SetActive(true);
+        }
+    }
+
+    [SerializeField] private Rigidbody[] rigidbodies;
+
+    private StartInfo[] startInfos;
+
+    void Awake()
+    {
+        startInfos = new StartInfo[rigidbodies.Length];
+        for (var i=0; i<rigidbodies.Length; i++)
+        {
+            startInfos[i] = new StartInfo(rigidbodies[i]);
+        }
+    }
+
+    public void Reset()
+    {
+        foreach (var startInfo in startInfos)
+        {
+            startInfo.Reset();
+        }
+    }
+}
--- oneDayGameClient/Assets/oneDayGame/Scripts/SampleController.cs
+++ oneDayGameClient/Assets/oneDayGame/Scripts/SampleController.cs
@@ -1,11 +1,45 @@
 ﻿using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class SampleController : MonoBehaviour
 {
     [SerializeField] private Rigidbody rigidbody;
 
-    public void MovePosition()
+    [SerializeField] private SphereCollider tipCollider;
+
+    [SerializeField] private SphereCollider targetBallCollider;
+
+    [SerializeField] private DragDetector dragDetector;
+
+    /// <summary>
+    /// dragの1pixelの移動で適用されるキューの加速度　
+    /// </summary>
+    private float speedCofficient = 0.1f;
+
+    void Awake()
     {
-        rigidbody.velocity = rigidbody.transform.forward * 50f;
+        dragDetector.OnDragCallback += OnDrag;
+    }
+
+    void OnDrag(PointerEventData eventData)
+    {
+        // Dragにより次のキューの位置
+        var nextQueuePosition = rigidbody.position + new Vector3(0, 0, eventData.delta.y * speedCofficient);
+
+        // tipの半径
+        var tipRadius = tipCollider.radius;
+
+        // ballの半径　
+        var ballRadius = targetBallCollider.radius;
+
+        var d = (targetBallCollider.transform.position - nextQueuePosition).sqrMagnitude;
+
+        // 接触する
+        if (d < Mathf.Pow(tipRadius + ballRadius, 2f))
+        {
+            rigidbody.velocity = new Vector3(0f, 0f, eventData.delta.y);
+        }
+
+        rigidbody.position = nextQueuePosition;
     }
 }
