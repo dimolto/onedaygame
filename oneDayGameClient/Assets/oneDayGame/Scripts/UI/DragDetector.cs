0ec5122ccc860d12d67783c69689cc2d62c1c41a
dimolto <oshiroyasu52@gmail.com>
2020-05-02 13:12:01 +0900

    add DragDetector

---
+++ oneDayGameClient/Assets/oneDayGame/Scripts/UI/DragDetector.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DragDetector : MonoBehaviour, IInitializePotentialDragHandler, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+    public Action<PointerEventData> OnInitializePotentialDragCallback;
+
+    public Action<PointerEventData> OnBeginDragCallback;
+
+    public Action<PointerEventData> OnDragCallback;
+
+    public Action<PointerEventData> OnEndDragCallback;
+
+    public void OnInitializePotentialDrag(PointerEventData eventData)
+    {
+        OnInitializePotentialDragCallback?.Invoke(eventData);
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        OnBeginDragCallback?.Invoke(eventData);
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        OnDragCallback?.Invoke(eventData);
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        OnEndDragCallback?.Invoke(eventData);
+    }
+}
---
+++ oneDayGameClient/Assets/oneDayGame/Scripts/UI/ImageClear.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ImageClear : Graphic
+{
+    protected override void OnPopulateMesh(VertexHelper vh)
+    {
+        vh.Clear();
+    }
+}
